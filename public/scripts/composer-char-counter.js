705174f71bd694fbca86f8ad3850d0115f6c4a8b
cphung1 <cphung97@gmail.com>
2020-02-25 23:44:31 +0000

    implemented character count, counter turns to red when negative

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,14 @@
+$(document).ready(() => {
+  $('textarea').on('input', function() {
+    let len = $(this).val().length;
+    let findCounter = $(this).parent().find('.counter');
+    let counter = findCounter.text(140 - len);
+
+    if (counter.text() < 0) {
+      counter.addClass("negativeNum")
+    } else {
+      counter.removeClass("negativeNum")
+    }
+
+  })
+});
