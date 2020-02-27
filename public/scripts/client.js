3b0ad71e1862b960e8e4d2454f5c02ae3d194bff
cphung1 <cphung97@gmail.com>
2020-02-27 00:56:57 +0000

    wehn submitting new tweet, renders new one on top, prevented XSS from user input in the form

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,16 +5,14 @@
  */
 
 $(function() {  
-  // loops through tweets
-  // calls createTweetElement for each tweet
-    // takes return value and appends it to the tweets container
-  const renderTweets = function(tweets) {
-    for (const elements of tweets) {
-      let value = createTweetElement(elements)
-      $('.tweets-container').append(value)
-    }
+
+  const escape =  function(str) {
+    let div = document.createElement('div');
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
   }
-  
+
+  //Creates the Tweet 
   const createTweetElement = function(tweet) {
     let time = moment(tweet["created_at"]).fromNow();
     let $tweet = $(`
@@ -28,37 +26,58 @@ $(function() {
            <p>${tweet["user"]["handle"]}</p>
           </span>
         </header>
-        <p class="tweetText">${tweet["content"]["text"]}</p>
+        <p class="tweetText">${escape(tweet["content"]["text"])}</p>
         <footer>
           <h6>${time}</h6>
         </footer>
       </article>
       `).addClass('tweet');
   
-    return $tweet
+    return $tweet;
   }
 
+  // empties the tweets container
+  // loops through tweets
+  // calls createTweetElement for each tweet
+  // takes return value and appends it to the tweets container
+  const renderTweets = function(tweets) {
+    $('.tweets-container').empty();
+    for (const elements of tweets) {
+      let value = createTweetElement(elements);
+      $('.tweets-container').prepend(value);
+    }
+  }
+
+  // get all tweet objects and reders them 
+  const loadTweets = function() {
+    $.ajax('/tweets', { method: 'GET'})
+      .done(function(arrayOfTweets) {
+        renderTweets(arrayOfTweets);
+        $('.new-tweet form').find('textarea').val('');
+      })
+  }
+
+  // upon clicking submit form checks if valid input
+  // if valid input loads new tweet on page
   $('.new-tweet form').submit(function() {
     event.preventDefault();
     let $inputLen = $(this).text();
     let $inputText = $(this).find('textarea').val().trim();
     if ($inputText === '' || $inputText === null) {
       alert("Cannot be left blank")
+      $(this).find('textarea').focus();
     } else if ($inputLen < 0) {
       alert("Input is too long")
+      $(this).find('textarea').focus();
     } else {
       $.ajax('/tweets', { method: 'POST', data: $(this).serialize() })
+        .done(function() {
+          loadTweets();
+        })
     }
-
   })
 
-  const loadTweets = function() {
-    $.ajax('/tweets', { method: 'GET'})
-      .done(function(arrayOfTweets) {
-      renderTweets(arrayOfTweets)
-      })
-  }
-  
+  // inital load of all the tweets 
   loadTweets();
 
 });
