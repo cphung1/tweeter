6aa54ab771520a9559f9f136e2c2b486a214dd91
cphung1 <cphung97@gmail.com>
2020-02-27 06:57:36 +0000

    when scrolling write new tweet button is diabled, shows up when back to top of window

--- public/scripts/compose-btn.js
+++ public/scripts/compose-btn.js
@@ -1,18 +1,22 @@
 // toggle button for composing new tweet
 $(function() {  
 
-
+  // toggles the compose tweet section 
   $('#new-tweet-btn').click(function() {
     $('.new-tweet').slideToggle();
   });
 
-  // $(window).scroll(function() {
-  //   if($(this).scrollTop() > 150) {
-  //     $btn.addClass('show')
-  //   } else {
-  //     $btn.removeClass('show')
-  //   }
-  // });
+  // if scrolling unable to click compose tweet button; button hides
+  // if at top them able to click compose tweet button; button shows
+  $(window).scroll(function() {
+    if($(this).scrollTop() > 150) {
+      // $('#new-tweet-btn').addClass('show')
+      $('#nav-new-tweet').addClass('hide')
+    } else {
+      // $('#new-tweet-btn').removeClass('show')
+      $('#nav-new-tweet').removeClass('hide')
+    }
+  });
 
 
 });
--- public/scripts/scroll-up-btn.js
+++ public/scripts/scroll-up-btn.js
@@ -1,5 +1,7 @@
 $(function() { 
 
+  // if scrolling back to top button will show
+  // if scrolling back to top button will hide
   $(window).scroll(function() {
     if($(this).scrollTop() > 150) {
       $('#back-to-top').addClass('show')
@@ -8,6 +10,7 @@ $(function() {
     }
   });
 
+  // if back to top button is clicked will bring back to top of window
   $('#back-to-top').click(function() {
     event.preventDefault();
     $('html, body').animate({scrollTop: 0}, 300);
